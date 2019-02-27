d7196d4cbaf051729b97649fa09304c096d1feb3
mmorehea <mmorehea@mix.wvu.edu>
2019-02-26 19:27:55 -0500

    first commit

---
+++ mesh2sy.py
@@ -0,0 +1,56 @@
+import pyglass
+import os
+import sys
+import glob
+import time
+import code
+import subprocess
+import ntpath
+import shutil
+from distutils.dir_util import copy_tree
+
+def checkForNewMeshes(oldListMeshes):
+	cwd = os.getcwd()
+	l = glob.glob(os.getcwd() + "\\meshes\\*")
+	for each in l:
+		if os.path.isdir(each) and each not in oldListMeshes:
+			print("Found new data set: " + each )
+			a = convertMeshes(each)
+			oldListMeshes.append(each)
+	return oldListMeshes
+	
+def copyEmpty(name):
+	emptyProject = "emptyProject/emptyProject.syg"
+	emptyProjectMeta = "emptyProject/emptyProject.sym"
+	if not os.path.exists("output/" + name):
+		os.mkdir("output/" + name)
+	shutil.copy(emptyProject, "output/" + name + "/" + name + ".syg")
+	shutil.copy(emptyProjectMeta, "output/" + name + "/" + name + ".sym")
+
+def convertMeshes(path):
+	name = ntpath.basename(path)
+	copyEmpty(name)
+	projectPath = "output/" + name + "/" + name + ".syg"
+	project = pyglass.OpenProject(pyglass.path(projectPath))
+	#code.interact(local=locals())
+	l = glob.glob(path + "\\*.obj")
+	project.ImportMeshOBJs("default", )
+	
+
+def main():
+	if not os.path.exists("output"):
+		os.mkdir("output")
+	if not os.path.exists("meshes"):
+		os.mkdir("meshes")
+	oldListMeshes = []#glob.glob(os.getcwd() + "\\meshes\\*")
+	while True:
+		print("Sleeping for a minute.")
+		time.sleep(1) 
+		print("Checking for new meshes files.")
+		oldListMeshes = checkForNewMeshes(oldListMeshes)
+		
+		
+
+
+
+if __name__ == "__main__": main()
